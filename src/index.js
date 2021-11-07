09751558d9396b0651d6d9674d462208604a8ec1
Taras Boikov <tarasgowest@gmail.com>
2021-11-07 22:40:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './sass/main.scss';
-import searchImage from'./js/apiService.js'
+import searchImage from './js/apiService.js'
 import inputArea from './partials/input.hbs';
 import pictureList from './partials/imagesList.hbs';
 import debounce from 'lodash.debounce';
